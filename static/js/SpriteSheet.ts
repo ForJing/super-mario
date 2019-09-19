be43c8ee9f2276294292d52012629aec1a2cfda7
forjing <xiaodoubao1994@gmail.com>
2019-09-20 00:26:39 +0800

    add define

--- static/js/SpriteSheet.ts
+++ static/js/SpriteSheet.ts
@@ -5,7 +5,15 @@ class SpriteSheet {
     this.tiles = new Map();
   }
 
-  define(name: string, x: number, y: number) {
+  define(name: string, x: number, y: number, w: number, h: number) {
+    const buffer = document.createElement("canvas");
+    buffer.width = w;
+    buffer.height = h;
+    buffer.getContext("2d").drawImage(this.image, x, y, w, h, 0, 0, w, h);
+    this.tiles.set(name, buffer);
+  }
+
+  defineTile(name: string, x: number, y: number) {
     const buffer = document.createElement("canvas");
     buffer.width = this.width;
     buffer.height = this.height;
--- static/js/main.ts
+++ static/js/main.ts
@@ -18,14 +18,31 @@ function drawBackground(
 const canvas = document.getElementById("screen") as HTMLCanvasElement;
 const context = canvas.getContext("2d");
 
-loadImage(require("../img/tiles.png")).then(image => {
-  const sprites = new SpriteSheet(image, 16, 16);
-  sprites.define("ground", 0, 0);
-  sprites.define("sky", 3, 23);
-
-  loadLevel("1-1").then(level => {
-    level.backgrounds.forEach(background => {
-      drawBackground(background, context, sprites);
-    });
+function loadMarioSprite() {
+  return loadImage("/img/characters.gif").then(image => {
+    const sprites = new SpriteSheet(image, 16, 16);
+    sprites.define("idle", 276, 44, 16, 16);
+    return sprites;
   });
+}
+
+function loadBackGroundSpirtes() {
+  return loadImage("/img/tiles.png").then(image => {
+    const sprites = new SpriteSheet(image, 16, 16);
+    sprites.defineTile("ground", 0, 0);
+    sprites.defineTile("sky", 3, 23);
+    return sprites;
+  });
+}
+
+Promise.all([
+  loadMarioSprite(),
+  loadBackGroundSpirtes(),
+  loadLevel("1-1")
+]).then(([marioSprite, sprites, level]) => {
+  level.backgrounds.forEach(background => {
+    drawBackground(background, context, sprites);
+  });
+
+  marioSprite.draw("idle", context, 64, 64);
 });
